959c242c5b83838b1f368e79ce2bafc00faeaff0
Gaurav Rawat <gauravrawat440@gmail.com>
2022-07-22 16:33:11 +0530

    fixed top characters page

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,6 +1,6 @@
 <script>
 	const name = 'Card';
-	export let character = {
+	export let cardDetailObj = {
 		image: 'https://cdn.myanimelist.net/images/characters/3/100534.jpg',
 		name: 'Zoro Roronoa',
 		desc: 'Zoro was the first crew member to be recruited by Luffy. Zoro is a skilled swordsman who fights with his own unique sword style known as santoryu',
@@ -9,16 +9,16 @@
 </script>
 
 <div class="card w-80 bg-gray-200 shadow-xl m-2 p-2">
-	<figure><img class="w-42" src={character.image} alt="Shoes" /></figure>
+	<figure><img class="w-42" src={cardDetailObj.image} alt="Shoes" /></figure>
 	<div class="card-body">
 		<h2 class="card-title">
-			{character.name}
+			{cardDetailObj.name}
 			<div class="badge badge-secondary">NEW</div>
 		</h2>
-		<p>{character.desc.substring(0, 100)}...</p>
+		<p>{cardDetailObj.desc.substring(0, 100)}...</p>
 		<div class="card-actions justify-end">
-			<div class="badge badge-outline">{character.badges[0]}</div>
-			<div class="badge badge-outline">{character.badges[1]}</div>
+			<div class="badge badge-outline">{cardDetailObj.badges[0]}</div>
+			<div class="badge badge-outline">{cardDetailObj.badges[1]}</div>
 		</div>
 	</div>
 </div>
